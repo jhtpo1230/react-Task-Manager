13689bab0faf3896989c6c1bc8e6e856a5d326e7
KimJeongChul <jhtpo1230@skuniv.ac.kr>
2025-11-17 20:58:54 +0900

    react+vite 첫 실습

---
+++ src/hooks/redux.ts
@@ -0,0 +1,6 @@
+import { useDispatch, useSelector } from 'react-redux';
+import type { TypedUseSelectorHook } from 'react-redux';
+import { AppDispatch, RootState } from "../store";
+
+export const useTypedSelector : TypedUseSelectorHook<RootState> = useSelector
+export const useTypedDispatch = () => useDispatch<AppDispatch>();
